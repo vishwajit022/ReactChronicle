8c5354212f58a8281350798f2a0d0bc03c20360c
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-03-22 04:10:19 +0530

    done

--- t1_countdowntimer/src/App.jsx
+++ t1_countdowntimer/src/App.jsx
@@ -1,37 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
 
 function App() {
-  const [start, isStart] = useState(false);
+  const [start, setStart] = useState(false);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const intervalRef = useRef();
+
   function handleStart() {
-    isStart(true);
+    setStart(true);
+    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    let remainingSeconds = totalSeconds;
+
+    intervalRef.current = setInterval(() => {
+      remainingSeconds--;
+      if (remainingSeconds < 0) {
+        clearInterval(intervalRef.current);
+        setStart(false);
+      } else {
+        const hours = Math.floor(remainingSeconds / 3600);
+        const minutes = Math.floor((remainingSeconds % 3600) / 60);
+        const seconds = remainingSeconds % 60;
+        setHours(hours);
+        setMinutes(minutes);
+        setSeconds(seconds);
+      }
+    }, 1000);
   }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+    setStart(false);
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  }
+
   return (
     <div>
-      <div className=" flex flex-col justify-center items-center h-screen bg-slate-500">
-        <h1 className="text-4xl">Countdown Timer</h1>
+      <div className="flex flex-col items-center justify-center h-screen bg-slate-500">
+        <h1 className="text-4xl font-medium ">Countdown Timer</h1>
         {!start && (
-          <div className="input-container mt-10 flex gap-2">
-            <input className="w-[50px] pl-2" type="number" placeholder="HH" />
-            <input className="w-[50px] pl-2" type="number" placeholder="MM" />
-            <input className="w-[50px] pl-2" type="number" placeholder="SS" />
+          <div className="flex gap-2 mt-10 input-container">
+            <input
+              className="w-[50px] pl-2"
+              type="number"
+              value={hours}
+              onChange={(e) => setHours(e.target.value)}
+              placeholder="HH"
+            />
+            <input
+              className="w-[50px] pl-2"
+              type="number"
+              value={minutes}
+              onChange={(e) => setMinutes(e.target.value)}
+              placeholder="MM"
+            />
+            <input
+              className="w-[50px] pl-2"
+              type="number"
+              value={seconds}
+              onChange={(e) => setSeconds(e.target.value)}
+              placeholder="SS"
+            />
           </div>
         )}
-        <div className="button mt-5 flex gap-10">
-          <button
+        <div className="flex gap-10 mt-5 button">
+          <motion.button
+            whileHover={{ scale: 1.12 }}
+            whileTap={{ scale: 0.94 }}
             onClick={handleStart}
             className="px-4 py-2 bg-green-400 rounded"
           >
             Start
-          </button>
-          <button className="px-4  py-2 bg-red-400 rounded">Stop</button>
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.12 }}
+            whileTap={{ scale: 0.94 }}
+            onClick={handleStop}
+            className="px-4 py-2 bg-red-400 rounded"
+          >
+            Stop
+          </motion.button>
         </div>
         {start && (
-          <div className="timer-box text-2xl flex mt-20">
-            <div className="">10</div>
+          <div className="flex mt-20 text-2xl timer-box">
+            <div>{hours}</div>
             <span>:</span>
-            <div className="">10</div>
+            <div>{minutes}</div>
             <span>:</span>
-            <div className="">10</div>
+            <div>{seconds}</div>
           </div>
         )}
       </div>
